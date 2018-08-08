c30911fc21768561bbc4ad69775208b22226ceb6
Tiago Vitorino <brother_87_22@hotmail.com>
2018-08-08 00:46:53 -0300

    versão melhorada - quase terminada - falta revisão

--- src/editor_de_variaveis_para_questaoconector_discursiva/Editor_de_variaveis_para_QuestaoConector_Discursiva.java
+++ src/editor_de_variaveis_para_questaoconector_discursiva/Editor_de_variaveis_para_QuestaoConector_Discursiva.java
@@ -20,22 +20,23 @@ import javax.swing.JOptionPane;
  * @author Win7
  */
 public class Editor_de_variaveis_para_QuestaoConector_Discursiva {
-public File abrirArquivo() {
+
+    public File abrirArquivo() {
         JFileChooser fc = new JFileChooser();
         int returnVal = fc.showOpenDialog(fc);
         File f = null;
         if (returnVal == JFileChooser.APPROVE_OPTION) {
             f = fc.getSelectedFile();
-        }else{
+        } else {
             System.exit(0);
         }
         return f;
     }
 
-@SuppressWarnings("empty-statement")
+    @SuppressWarnings("empty-statement")
     public void realizarLeituraDaLinhaDoArquivo(File file) throws FileNotFoundException, IOException {
         try (FileInputStream fis = new FileInputStream(file.getAbsolutePath())) {
-            Charset cs = Charset.forName("UTF-8");
+            Charset cs = Charset.forName("Cp1252");
             InputStreamReader isr = new InputStreamReader(fis, cs);
             int ch;
             int contador = 0;
@@ -43,73 +44,43 @@ public File abrirArquivo() {
             String text = "";
             File arquivoParaSalvar = selecionarLocalParaSalvar();
             while ((ch = isr.read()) != -1) {
-                if (ch != 10) {
-                    text += String.valueOf((char) ch);
+                if (ch != 13) {
+                    if (ch != 10) {
+                        text += String.valueOf((char) ch);
+                    }
                 } else {
-                    text += String.valueOf((char) ch);
-                    contador = processarLinha(text, arquivoParaSalvar, contador);
+                    contador = processarLinha2(text, arquivoParaSalvar, contador);
                     text = "";
-                }
+                } //text += String.valueOf((char) ch);
             }
         } catch (Exception ex) {
             JOptionPane.showMessageDialog(null, ex.toString());
         }
 
     }
-    
-    public int processarLinha(String text, File arquivo, int contador){
-        String text2 = "";
-        if (text.indexOf("e\">e)") > 0 || text.indexOf("e\">xe)") > 0) {            
-            if (text.indexOf("e\">xe)") > 0) {
-                text = text.replace("xe)", "e)");
-                text2 += "b[" + contador + "][5]=4;\n";
-                salvarLinhasProcessadas(arquivo.getAbsolutePath() + "/conector.java", text2);
-                text2 = "";
-            }            
-            String[] text3 = {"a", "b", "c", "d", "e"};
-            text2 += "a[" + contador + "]=R.string.q" + contador + ";\n";
-            for (int contador2 = 0; contador2 < 5; contador2++) {
-                text2 += "b[" + contador + "][" + contador2 + "]=R.string.q" + contador + text3[contador2] + ";\n";
-            }            
-            contador++;
-            //JOptionPane.showMessageDialog(null, text2);
-            salvarLinhasProcessadas(arquivo.getAbsolutePath() + "/conector.java", text2);
-        } else if (text.indexOf("xa)") > 0) {
-            text = text.replace("xa)", "a)");
-            text2 += "b[" + contador + "][5]=0;\n";
-            salvarLinhasProcessadas(arquivo.getAbsolutePath() + "/conector.java", text2);
-            //text=text.substring(0, text.indexOf("xa)")) + 
-            //        text.substring(text.indexOf("xa)")+1, text.length());
-        } else if (text.indexOf("xb)") > 0) {
-            text = text.replace("xb)", "b)");
-            text2 += "b[" + contador + "][5]=1;\n";
-            salvarLinhasProcessadas(arquivo.getAbsolutePath() + "/conector.java", text2);
-            //text=text.substring(0, text.indexOf("xb)")) + 
-            //    text.substring(text.indexOf("xb)")+1, text.length());
-        } else if (text.indexOf("xc)") > 0) {
-            text = text.replace("xc)", "c)");
-            text2 += "b[" + contador + "][5]=2;\n";
-            salvarLinhasProcessadas(arquivo.getAbsolutePath() + "/conector.java", text2);
-            //text=text.substring(0, text.indexOf("xc)")) + 
-            //    text.substring(text.indexOf("xc)")+1, text.length());
-        } else if (text.indexOf("xd)") > 0) {
-            text = text.replace("xd)", "d)");
-            text2 += "b[" + contador + "][5]=3;\n";
-            salvarLinhasProcessadas(arquivo.getAbsolutePath() + "/conector.java", text2);
-            //text=text.replace("xe)", "e)");
-            //text=text.substring(0, text.indexOf("xd)")) + 
-            // text.substring(text.indexOf("xd)")+1, text.length());
-        } else if (text.indexOf("xe)") > 0) {
-//                            text=text.replace("xe)", "e)");                            
-//                            text2+="b["+contador+"][5]=4\n";  
-//                            salvarLinhasProcessadas(arquivo.getAbsolutePath()+"/conector.java", text2); 
-        }
 
-        //JOptionPane.showMessageDialog(null, text);
+    public int processarLinha(String text, File arquivo, int contador) {
+        if (text.contains("Resp.:")) {
+            text = "a[" + contador + "]=R.string.q" + contador + ";\n";
+            text += "b[" + contador + "][0]=R.string.q" + contador + "a;\n";            
+            salvarLinhasProcessadas(arquivo.getAbsolutePath() + "/conector.java", text);
+        }
+        return contador;
+    }
+
+    public int processarLinha2(String text, File arquivo, int contador) {
+        String textAux = text;        
+        if (text.contains("Resp.:")) {
+            text = "<string name=\"q" + contador + "a\">" + text + "</string>\n";
+            processarLinha(textAux, arquivo, contador);
+            contador++;
+        }else{
+            text = "<string name=\"q" + contador + "\">" + text + "</string>\n";
+        }
         salvarLinhasProcessadas(arquivo.getAbsolutePath() + "/questoes.xml", text);
         return contador;
     }
-    
+
     public void salvarLinhasProcessadas(String file, String textoParaSalvar) {
         try {
             if (file != null) {
@@ -128,7 +99,7 @@ public File abrirArquivo() {
         JOptionPane.showMessageDialog(null, "Escolha o local onde salvar");
         fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
         int returnval = fc.showSaveDialog(fc);
-        File file=null;
+        File file = null;
         if (returnval == JFileChooser.APPROVE_OPTION) {
             file = fc.getSelectedFile();
         } else {
@@ -137,12 +108,22 @@ public File abrirArquivo() {
         }
         return file;
     }
-    
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        JOptionPane.showMessageDialog(null, "Esse projeto consiste em criar dois arquivos: "
+                + "questao.xml e conector.java\n\nPara usá-lo corretamente siga os seguintes passos:\n"
+                + "1º Crie as questões num arquivo qualquer seguindo o padrão Questionation (padrão próprio);\n"
+                + "2º Utilizando o Notepad++ rode a macro \"string name q 0 a 9\" para complementar o arquivo;\n"
+                + "3º É importante lembrar de colocar x na frente das alternativas que são as corretas. Se não tiver feito faça-o agora\n");
+        JOptionPane.showMessageDialog(null, "Escolha o arquivo");
+        Editor_de_variaveis_para_QuestaoConector_Discursiva editor = new Editor_de_variaveis_para_QuestaoConector_Discursiva();
+        File file = editor.abrirArquivo();
+        editor.realizarLeituraDaLinhaDoArquivo(file);
+        JOptionPane.showMessageDialog(null, "O programa terminou o serviço!");
     }
-    
+
 }
